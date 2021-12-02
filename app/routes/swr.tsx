de1e8371f74f0c53869afd39590bee4ac9ca2dee
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2021-12-02 09:16:47 +0900

    fix swr test file

--- app/routes/swr.tsx
+++ app/routes/swr.tsx
@@ -1,6 +1,11 @@
 import { HeadersFunction, LoaderFunction, useLoaderData } from 'remix';
 
 export const headers: HeadersFunction = () => {
+  // max-age=0 ブラウザにキャッシュさせない
+  // s-maxage=30 vercel側の最大キャッシュ時間
+  // state-while-revalidate=30 30秒経過後新しいアクセスがあった場合、
+  // そのアクセスに対してはキャッシュを渡し、バックグラウンドでは新規生成を行う
+  // 生成完了後にキャシュを入れ替える
   return {
     'Cache-Control': 'max-age=0, s-maxage=30, state-while-revalidate=30',
   };
